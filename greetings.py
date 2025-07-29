8e6180987ccd0eafacf49c81d3e7ac35eb339b3e
JeffuMista <kariukigeoffrey18@gmail.com>
2025-07-29 12:46:46 +0300

    initial repository

---
+++ greetings.py
@@ -0,0 +1,5 @@
+name = str(input("Please enter your name: "))
+
+color = str(input("Please enter your favorite color: "))
+
+print(f"Hello {name}, your favorite color is {color}. Bold choice mate!")
---
+++ index.py
@@ -0,0 +1,8 @@
+name = "Jeffu Mista"
+
+age = 30
+
+occupation = "Software Engineer"
+
+stack = ["Python", "JavaScript", "React"]
+print(f"Hello there, my name is {name}, I am {age} years old. I am a {occupation} and I work with the following technologies: {', '.join(stack)}.")
